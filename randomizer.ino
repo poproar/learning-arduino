427b835fd5077c1a0e7c3bf681439e63a6d82a59
Andrew Thomas <poproar@users.noreply.github.com>
2019-04-23 22:39:12 -0700

    test blinking light with slow down

--- randomizer.ino
+++ randomizer.ino
@@ -1,11 +1,17 @@
 int speed = 0 ;
 int spintime = 0 ;
+int start = 0 ;
+int end = 0 ;
+int trip = 0 ;
+int cycle = 100;
+bool led= false ;
 
 void setup()
 {
   pinMode( 2 , INPUT);
   pinMode( 3 , OUTPUT);
   Serial.begin(9600);
+  pinMode( 7 , OUTPUT);
 }
 
 void loop()
@@ -14,12 +20,27 @@ void loop()
   {
     speed = ( 150 + 	random( 0 , 100 ) ) ;
     spintime = ( 1000 * 	random( 1 , 10 ) ) ;
+    start = millis() ;
+    end = ( start + spintime ) ;
+    trip = ( start + cycle ) ;
     Serial.print("speed: ");
     Serial.println(speed);
     Serial.print("spintime: ");
     Serial.println(spintime);
     analogWrite(3 , speed);
-    delay( spintime );
+    
+    while ( millis()  < end )
+    {
+      if (( ( millis() ) >= ( trip ) ))
+      {
+        trip = ( trip + cycle ) ;
+        led = !led ;
+        speed -= 10;
+        digitalWrite( 7 , led );
+        analogWrite(3 , speed);
+      }
+    }
   }
+  digitalWrite( 7, LOW );
+  analogWrite(3, 0);
 }
-
