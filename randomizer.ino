833887ca8cd406774e80c9b047a28a93a9631c20
Andrew Thomas <poproar@users.noreply.github.com>
2019-05-10 18:43:42 -0700

    Update randomizer.ino

--- randomizer.ino
+++ randomizer.ino
@@ -1,46 +1,89 @@
-int speed = 0 ;
-int spintime = 0 ;
-int start = 0 ;
-int end = 0 ;
-int trip = 0 ;
-int cycle = 100;
-bool led= false ;
+unsigned long spintime, start, result, trip = 0;
+unsigned int cycle = 0;
+byte i, spin = 0;
 
-void setup()
-{
-  pinMode( 2 , INPUT);
-  pinMode( 3 , OUTPUT);
+void setup() {
+  //start serial connection
   Serial.begin(9600);
-  pinMode( 7 , OUTPUT);
+  //configure pin2 as an input and enable the internal pull-up resistor
+  pinMode(2, INPUT_PULLUP);
+  pinMode(13, OUTPUT);
+
 }
 
 void loop()
 {
-  if ( digitalRead(2) == HIGH )
+  if ( digitalRead(2) == LOW )
   {
-    speed = ( 150 + 	random( 0 , 100 ) ) ;
-    spintime = ( 1000 * 	random( 1 , 10 ) ) ;
+    cycle = 100;
+    spin = ( 180 +   random( 0 , 75 ) ) ;
+    spintime = ( 1000 *   random( 1 , 10 ) ) ;
     start = millis() ;
-    end = ( start + spintime ) ;
+    result = ( start + spintime ) ;
     trip = ( start + cycle ) ;
-    Serial.print("speed: ");
-    Serial.println(speed);
-    Serial.print("spintime: ");
-    Serial.println(spintime);
-    analogWrite(3 , speed);
+    Serial.print("start: ");
+    Serial.print(start);
+    Serial.print(" - end: ");
+    Serial.print(result);
+    Serial.print(" - speed: ");
+    Serial.print(spin);
+    Serial.print(" - spintime: ");
+    Serial.print(spintime);
+    analogWrite(3 , spin);
     
-    while ( millis()  < end )
+    while ( millis()  < result )
     {
       if (( ( millis() ) >= ( trip ) ))
       {
+        i++;
+        if (i == 8 )
+        {
+          cycle *= 2;
+          i = 0;
+        }
+        
         trip = ( trip + cycle ) ;
-        led = !led ;
-        speed -= 10;
-        digitalWrite( 7 , led );
-        analogWrite(3 , speed);
+        Serial.print(" - trip: ");
+        Serial.print(trip);
+        Serial.print(" - cycle: ");
+        Serial.print(cycle);
+        Serial.print(" - i: ");
+        Serial.print(i);
+        Serial.print(" ");
+        spin -= 10;
+
+        
+        analogWrite( 9, bitRead(i,2)*255 ); //blue
+        analogWrite( 6, bitRead(i,1)*255 ); //green
+        analogWrite( 5, bitRead(i,0)*255 ); //red
+        
+        Serial.print(bitRead(i,2));
+        Serial.print(bitRead(i,1));
+        Serial.println(bitRead(i,0));
+
       }
     }
+    Serial.print(" RESULT v TRIP?: ");
+    Serial.print(result);
+    Serial.print(" - v - : ");
+    Serial.print(trip);
+    Serial.print(" - f spin : ");
+    Serial.print(spin);
+    
+      byte flash = i*5;
+      for(byte j = 0; j<5 ; j++)
+      {
+        analogWrite( 9, 0 ); //blue
+        analogWrite( 6, 0 ); //green
+        analogWrite( 5, 0 ); //red
+        delay(50);
+        analogWrite( 9, bitRead(i,2)*255 ); //blue
+        analogWrite( 6, bitRead(i,1)*255 ); //green
+        analogWrite( 5, bitRead(i,0)*255 ); //red
+        delay(50);
+      }
+      analogWrite(3, 0);
+    
   }
-  digitalWrite( 7, LOW );
-  analogWrite(3, 0);
+
 }
